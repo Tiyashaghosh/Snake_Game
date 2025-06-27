35a4810eac7fc2b87b2e88aa832f53c0314a7553
Tiyashaghosh <149707689+Tiyashaghosh@users.noreply.github.com>
2025-06-27 10:31:17 +0530

    Update score.py
    Added high score to the game.

--- score.py
+++ score.py
@@ -6,21 +6,24 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score =0
         self.color("white")
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
         self.update_score()
-    def update_score(self):
-        self.write(f"Score : {self.score}", align=ALIGN, font=FONT)
 
-    def game_over(self):
+    def update_score(self):
         self.clear()
-        self.goto(0,0)
-        self.write(f"Game Over.\n Score : {self.score}", align=ALIGN, font=FONT)
+        self.write(f"Score : {self.score}  High Score : {self.high_score}", align=ALIGN, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_score()
 
     def increase(self):
-        self.clear()
         self.score += 1
         self.update_score()
 
